5bf8ddd5eec53f1cdf970d542a4dd506e8d3279a
Adrian E Gil <adrianegil97@gmail.com>
2023-02-08 10:41:08 -0500

    Added optimization and version management

--- app/src/main/java/cu/desoft/filhabana/ui/fragments/AboutFragment.kt
+++ app/src/main/java/cu/desoft/filhabana/ui/fragments/AboutFragment.kt
@@ -1,13 +1,17 @@
 package cu.desoft.filhabana.ui.fragments
 
+import android.content.Intent
+import android.graphics.Paint
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import cu.desoft.filhabana.ui.activities.MainActivity
+import cu.desoft.filhabana.BuildConfig
 import cu.desoft.filhabana.R
 import cu.desoft.filhabana.databinding.FragmentAboutBinding
+import cu.desoft.filhabana.ui.activities.MainActivity
 
 class AboutFragment : Fragment() {
 
@@ -23,8 +27,19 @@ class AboutFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.textViewAppVersion.text = "Versión: ${BuildConfig.VERSION_NAME}"
+
         binding.btnDescription.setOnClickListener {
             MainActivity.navController.navigate(R.id.go_descriptionFragment)
+        }
+
+        //binding.textFILHabanaSiteWeb.setPaintFlags(binding.textFILHabanaSiteWeb.getPaintFlags() or Paint.UNDERLINE_TEXT_FLAG)
+
+        binding.textFILHabanaSiteWeb.setOnClickListener {
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://www.filhcuba.cu/"))
+            startActivity(browserIntent)
         }
     }
 
